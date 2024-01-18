63ce808088923a7fce9939790cb47ea65b97304b
joshrmcdaniel <80354972+joshrmcdaniel@users.noreply.github.com>
2024-01-17 19:31:07 -0600

    working? implementation

---
+++ README.md
@@ -0,0 +1,7 @@
+# sng-format-python
+-----------------------
+Python implementation for parsing and handling .sng files. [See the .sng format spec for information](https://github.com/mdsitton/SngFileFormat/tree/main).
+
+It is capable of reading SNG files, extracting metadata, file indexes, and file data. There is a function to write to disk as well.
+
+I have not tested this thoroughly, but I've had zero issues with it so far.
---
+++ sng.py
@@ -0,0 +1,100 @@
+import struct
+from collections import namedtuple
+import os
+from configparser import ConfigParser
+
+
+SngMetadata = namedtuple('SngMetadata', ['filename', 'content_len', 'content_idx'])
+
+
+def unmask(data, xor_mask):
+    unmasked_data = bytearray(len(data))
+    for i in range(len(data)):
+        xor_key = xor_mask[i % 16] ^ (i & 0xFF)
+        unmasked_data[i] = data[i] ^ xor_key
+    return unmasked_data
+
+
+def read_metadata(buffer, offset):
+    filename_len = struct.unpack_from('<B', buffer, offset)[0]
+    offset += 1
+    filename = struct.unpack_from(f'<{filename_len}s', buffer, offset)[0].decode()
+    offset += filename_len
+    contents_len, contents_index = struct.unpack_from('<QQ', buffer, offset)
+    offset += 16
+    return SngMetadata(filename, contents_len, contents_index), offset
+
+
+def parse_sng(sng_buffer):
+    file_identifier, version, xor_mask = struct.unpack('<6sI16s', sng_buffer[:26])
+    assert file_identifier in bytes([0x53, 0x4E, 0x47, 0x50, 0x4b, 0x47]), "Not a song file."
+
+    if file_identifier.decode() != "SNGPKG":
+        raise ValueError("Invalid file identifier")
+
+    metadata_len, metadata_count = struct.unpack_from('<QQ', sng_buffer, 26)
+
+    offset = 26 + 16
+
+    metadata = {}
+    
+    for _ in range(metadata_count):
+        key_len = struct.unpack_from('<I', sng_buffer, offset)[0]
+        offset += 4
+        key = struct.unpack_from(f'<{key_len}s', sng_buffer, offset)[0].decode()
+        offset += key_len
+        value_len = struct.unpack_from('<I', sng_buffer, offset)[0]
+        offset += 4
+        value = struct.unpack_from(f'<{value_len}s', sng_buffer, offset)[0].decode()
+        offset += value_len
+        metadata[key] = value
+
+    file_meta_len, file_count = struct.unpack_from('<QQ', sng_buffer, offset)
+    print(file_meta_len)
+    offset += 16
+    file_meta_array = []
+    for _ in range(file_count):
+        file_meta, new_offset = read_metadata(sng_buffer, offset)
+        file_meta_array.append(file_meta)
+        offset = new_offset
+
+    file_data_len = struct.unpack_from('<Q', sng_buffer, offset)[0]
+    offset += 8
+    file_data_array = []
+    for file_meta in file_meta_array:
+        file_data = sng_buffer[offset:offset+file_meta.content_len]
+        file_data = unmask(file_data, xor_mask)
+        file_data_array.append(file_data)
+        offset += file_meta.content_len
+
+    return {
+        'file_identifier': file_identifier.decode(),
+        'version': version,
+        'xor_mask': xor_mask,
+        'metadata': metadata,
+        'file_meta_array': file_meta_array,
+        'file_data_array': file_data_array
+    }
+
+
+def write_parsed_sng(parsed_data, outdir):
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    cfg = ConfigParser()
+    cfg.add_section('Song')
+    for k, v in parsed_data['metadata'].items():
+        cfg.set('Song', k, v)
+    with open(os.path.join(outdir, 'song.ini'), 'w') as f:
+        cfg.write(f)
+
+    for index, file_meta in enumerate(parsed_data['file_meta_array']):
+        file_data = parsed_data['file_data_array'][index]
+        file_path = os.path.join(outdir, file_meta.filename)
+        with open(file_path, 'wb') as file:
+            file.write(file_data)
+
+
+def read_sng_file(path: str):
+    with open(path, 'rb') as f:
+        d = f.read()
+    return d
