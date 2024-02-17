f7f31c89a5a93602657eca75f978d54c56d5cfbf
joshrmcdaniel <80354972+joshrmcdaniel@users.noreply.github.com>
2024-02-17 16:22:24 -0600

    rm print

--- src/sng_parser/audio/convert.py
+++ src/sng_parser/audio/convert.py
@@ -4,8 +4,6 @@ import soundfile as sf
 def to_opus(filepath: str) -> BytesIO:
     filename, ext = filepath.split('.')
     with sf.SoundFile(filepath, 'r') as f:
-        print(f.channels)
-        print(vars(f))
         out = BytesIO()
         out.name = filename+'.opus'
         with sf.SoundFile(out, 'w', samplerate=f.samplerate, channels=f.channels, format='ogg', subtype='Opus', endian=f.endian) as g:
